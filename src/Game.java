f71f965fe88a3015e9ee095edc9b9d597c1e0c76
Colin Hayes <colin.hayes985@gmail.com>
2018-07-02 11:43:53 -0400

    Added a method to randomly add tiles to the board and called it twice to start the game

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args){
         int [][] grid = new int[4][4];
+        placeTile(grid);
+        placeTile(grid);
         print(grid);
+
     }
     public static void print(int[][] array) {
         for (int i = 0; i < 4; i++) {
@@ -13,6 +16,23 @@ public class Game {
             }
             System.out.println("");
         }
+    }
+    public static void placeTile(int[][] array) {
+        Random rand = new Random();
+        int i = rand.nextInt(4);
+        int j = rand.nextInt(4);
+        while (array[i][j] != 0) {
+            i = rand.nextInt(4);
+            j = rand.nextInt(4);
+        }
+        int num = rand.nextInt(5);
+        if (num == 0) {
+            array[i][j]=4;
+        }
+        else{
+            array[i][j]=2;
+        }
+
     }
 }
 
