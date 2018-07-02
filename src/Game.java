df81de1b50542eb1e6c3c078fdd5ff75bd35653c
Colin Hayes <colin.hayes985@gmail.com>
2018-07-02 11:12:35 -0400

    Format board to print with 4 digit numbers so it stays aligned

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
+    public static void main(String[] args){
+        int [][] grid = new int[4][4];
+        print(grid);
+    }
+    public static void print(int[][] array) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.format("%04d ", array[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
+
